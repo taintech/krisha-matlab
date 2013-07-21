d3e01d27b973eeb65439caae72557c6b968430f3
taintech <rinattainov@gmail.com>
2013-07-21 13:51:56 +0600

    resolve with unicode problem

--- db.m
+++ db.m
@@ -1,9 +1,8 @@
-dbname = 'krisha';
 username = 'krisha';
 password = 'krisha';
 driver = 'com.mysql.jdbc.Driver';
-dburl = ['jdbc:mysql://localhost:3306/' dbname];
+dburl = 'jdbc:mysql://localhost:3306/krisha?useUnicode=true&characterEncoding=UTF-8';
 
 javaclasspath('/Users/taintech/.m2/repository/mysql/mysql-connector-java/5.1.25/mysql-connector-java-5.1.25.jar');
 
-conn = database(dbname, username, password, driver, dburl);
+conn = database('', username, password, driver, dburl);
